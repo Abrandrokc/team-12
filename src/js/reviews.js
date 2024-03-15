2e2f6bf7839a08a06e10e5f1656719b98583f42c
Abrandrokc <Abrandrokco@gmail.com>
2024-03-15 20:56:11 +0200

    12

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
+import Swiper from 'swiper';
+import 'swiper/swiper-bundle.css';
+
 axios.defaults.baseURL = 'https://portfolio-js.b.goit.study/api';
 
 export const reviewWrapper = document.querySelector('.reviews-swiper-wrapper');
+const reviewsSwiper = document.querySelector('.reviews-swiper')
 
 export async function fetchData() {
     try {
@@ -16,11 +20,19 @@ export async function fetchData() {
 export async function renderSlide() {
     let data = await fetchData();
     if (data) {
-        const html = Array.from(data).map(({ author, avatar_url, review }) => `<li class='swiper-slide'>
+        const html = data.map(({ author, avatar_url, review }) => `<li class='review-swiper-slide'>
             <img class="review-img" src="${avatar_url}" alt="${author}" />
             <h3 class="review-author">${author}</h3>
             <p class="review-text">${review}</p>
         </li>`).join('');
-        reviewWrapper.insertAdjacentHTML("afterbegin", html);
+
+      
+        reviewWrapper.insertAdjacentHTML('afterbegin', html)
+
+
+       
     }
 }
+
+
+   
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
-import { renderSlide } from './js/reviews';
+import { renderSlide,  } from './js/reviews';
+import Swiper from 'swiper';
+import 'swiper/swiper-bundle.css';
 
 axios.defaults.baseURL = 'https://portfolio-js.b.goit.study/api';
 
-renderSlide();
+
+renderSlide(); 
+  
