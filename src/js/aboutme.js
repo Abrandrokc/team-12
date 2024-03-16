ec1c1f2f52097a61ff2d91d6338cb7a910cb1557
Artem0202 <artyomkov.19@gmail.com>
2024-03-16 21:07:06 +0200

    Update js

--- src/js/aboutme.js
+++ src/js/aboutme.js
@@ -1,4 +1,16 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
-new Accordion('.info-list');
+const buttonAccordion = document.querySelector('.button-up-down');
+
+new Accordion('.info-list', {
+  duration: 700,
+  showMultiple: true,
+  openOnInit: [0],
+  onOpen: currentElement => {
+    console.log(currentElement);
+  },
+  onClose: currentElement => {
+    console.log(currentElement);
+  },
+});
