23f23b4ce75d359d4f25b440f94b8a51d0994afb
Maxym87 <112802810+Maxym87@users.noreply.github.com>
2024-03-17 10:46:58 +0200

    adds logo img

--- src/js/header.js
+++ src/js/header.js
@@ -2,3 +2,31 @@ const menuBtn = document.querySelector('.header-nav__menu');
 const openMenuBtn = document.querySelector('.menu-open');
 const closeMenuBtn = document.querySelector('.menu-close');
 const menuList = document.querySelector('.header-list');
+
+function openMenu() {
+  menuList.classList.remove('is-hidden');
+  closeMenuBtn.style.display = 'block';
+  openMenuBtn.style.display = 'none';
+}
+
+function closeMenu() {
+  menuList.classList.add('is-hidden');
+  closeMenuBtn.style.display = 'none';
+  openMenuBtn.style.display = 'block';
+}
+
+menuBtn.addEventListener('click', event => {
+  if (menuList.classList.contains('is-hidden')) {
+    menuList.classList.remove('is-hidden');
+  } else {
+    menuList.classList.add('is-hidden');
+  }
+});
+
+openMenuBtn.addEventListener('click', event => {
+  openMenu();
+});
+
+closeMenuBtn.addEventListener('click', event => {
+  closeMenu();
+});
