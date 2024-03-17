d7d9d4a1aff18b117a4d95d97c8f6912a79159d3
Nazar Koshytskyi <77495036+NazarKoshitsky@users.noreply.github.com>
2024-03-17 21:46:03 +0100

    finally
    finall FAQ

--- src/js/FAQ.js
+++ src/js/FAQ.js
@@ -1,7 +1,7 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
-const showAcc = new Accordion('.accordion-container', {
+const SHOWACC = new Accordion('.accordion-container', {
     showMultiple: true,
 });
-export default showAcc;
+export default SHOWACC;
