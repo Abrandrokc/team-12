912ea2077e13e297c9314857ec24f29f4b6e7791
Nazar Koshytskyi <77495036+NazarKoshitsky@users.noreply.github.com>
2024-03-17 20:12:14 +0100

    upd js
    make new Branch and create Faq JS

---
+++ src/js/FAQ.js
@@ -0,0 +1,7 @@
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+
+const showAcc = new Accordion('.accordion-container', {
+    showMultiple: true,
+});
+export default showAcc;
