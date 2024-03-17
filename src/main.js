44c118ef65df2b7469ae112aacf06ec7ecb907ff
Abrandrokc <Abrandrokco@gmail.com>
2024-03-17 11:16:00 +0200

    acord

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { renderSlide,  } from './js/reviews';
 import Swiper from 'swiper';
 import 'swiper/swiper-bundle.css';
+import itemsAccordion from './js/aboutme';
 
 axios.defaults.baseURL = 'https://portfolio-js.b.goit.study/api';
 
