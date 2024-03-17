98d8cd24dd7870948eff90d8c50fd6dcfe40d90e
Maxym87 <112802810+Maxym87@users.noreply.github.com>
2024-03-17 13:56:00 +0200

    adds styles media 768 & 1280

--- src/js/header.js
+++ src/js/header.js
@@ -1,32 +1,32 @@
-const menuBtn = document.querySelector('.header-nav__menu');
-const openMenuBtn = document.querySelector('.menu-open');
-const closeMenuBtn = document.querySelector('.menu-close');
-const menuList = document.querySelector('.header-list');
+// const menuBtn = document.querySelector('.header-nav__menu');
+// const openMenuBtn = document.querySelector('.menu-open');
+// const closeMenuBtn = document.querySelector('.menu-close');
+// const menuList = document.querySelector('.header-list');
 
-function openMenu() {
-  menuList.classList.remove('is-hidden');
-  closeMenuBtn.style.display = 'block';
-  openMenuBtn.style.display = 'none';
-}
+// function openMenu() {
+//   menuList.classList.remove('is-hidden');
+//   closeMenuBtn.style.display = 'block';
+//   openMenuBtn.style.display = 'none';
+// }
 
-function closeMenu() {
-  menuList.classList.add('is-hidden');
-  closeMenuBtn.style.display = 'none';
-  openMenuBtn.style.display = 'block';
-}
+// function closeMenu() {
+//   menuList.classList.add('is-hidden');
+//   closeMenuBtn.style.display = 'none';
+//   openMenuBtn.style.display = 'block';
+// }
 
-menuBtn.addEventListener('click', event => {
-  if (menuList.classList.contains('is-hidden')) {
-    menuList.classList.remove('is-hidden');
-  } else {
-    menuList.classList.add('is-hidden');
-  }
-});
+// menuBtn.addEventListener('click', event => {
+//   if (menuList.classList.contains('is-hidden')) {
+//     menuList.classList.remove('is-hidden');
+//   } else {
+//     menuList.classList.add('is-hidden');
+//   }
+// });
 
-openMenuBtn.addEventListener('click', event => {
-  openMenu();
-});
+// openMenuBtn.addEventListener('click', event => {
+//   openMenu();
+// });
 
-closeMenuBtn.addEventListener('click', event => {
-  closeMenu();
-});
+// closeMenuBtn.addEventListener('click', event => {
+//   closeMenu();
+// });
