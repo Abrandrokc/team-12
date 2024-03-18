c5ed9e0ef69d7581db0471009f7c4ab82d885a55
Maxym87 <112802810+Maxym87@users.noreply.github.com>
2024-03-18 19:58:49 +0200

    adds hover & focus

--- src/js/header.js
+++ src/js/header.js
@@ -1,37 +1,34 @@
 const mobMenuItems = document.querySelectorAll('.header-list .header-link');
 const menuOpenBtn = document.querySelector('.menu-open');
 const menuCloseBtn = document.querySelector('.menu-close');
-const mobMenuContainer = document.querySelector('.header-mob__menu');
+const mobMenuContainer = document.querySelector('.mobile-menu');
 const orderBtn = document.querySelector('.header-order');
 const headerMenuBtn = document.querySelector('.header-nav__menu');
 const headerList = document.querySelector('.header-list');
+const markup = document.querySelector('.markup');
 
 export function createMarkup() {
   const items = [...mobMenuItems];
-  const mobMenuListHTML = `
-       <ul class="mob-menu__list">
-      ${items
-        .map(
-          item =>
-            `<li class="mob-menu__item"><a class="mob-menu__link" href="${item.href}">${item.textContent}</a></li>`
-        )
-        .join('')}
-    </ul>
-  `;
+  const mobMenuListHTML = items
+    .map(
+      item =>
+        `<li class="mob-menu__item"><a class="mob-menu__link" href="${item.href}">${item.textContent}</a></li>`
+    )
+    .join('');
 
-  mobMenuContainer.innerHTML = mobMenuListHTML;
+  markup.innerHTML += `<ul class="mob-menu__list">${mobMenuListHTML}</ul>`;
 }
 
 export function openMenu() {
   menuOpenBtn.classList.add('is-hidden');
   menuCloseBtn.classList.remove('is-hidden');
+  mobMenuContainer.classList.add('is-open');
   createMarkup();
-  mobMenuContainer.classList.remove('is-hidden');
   orderBtn.style.display = 'block';
 }
 
 export function closeMenu() {
-  mobMenuContainer.classList.add('is-hidden');
+  mobMenuContainer.classList.remove('is-open');
   menuOpenBtn.classList.remove('is-hidden');
   menuCloseBtn.classList.add('is-hidden');
   orderBtn.style.display = 'none';
@@ -42,4 +39,5 @@ menuCloseBtn.addEventListener('click', closeMenu);
 
 headerMenuBtn.addEventListener('click', event => {
   headerList.classList.toggle('is-hidden');
+  headerList.style.opacity = 1;
 });
