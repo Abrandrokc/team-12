3ac4f066e0de7799fd505a6f5486879bbb78ae99
klimisnotavailable <klimgrigor28@gmail.com>
2024-03-18 22:21:27 +0200

    js

--- src/main.js
+++ src/main.js
@@ -39,32 +39,8 @@ document.addEventListener('keydown', escapeClose);
 
 
 
-// covers code
+const element = document.getElementById('your-element-id');
 
-
-
-// const covers = document.querySelector(".section.covers")
-// window.visualViewport.addEventListener("scroll",(event)=> {
-//     // console.log(event)
-//     // console.log(covers.getBoundingClientRect())
-//     isElementInViewport(covers)
-// });
-
-// function isElementInViewport(el) {
-//     const rect = el.getBoundingClientRect();
-//     console.log(visualViewport)
-//     console.log(rect.top)
-//     return (
-//         rect.top >= 0 &&
-//         rect.left >= 0 &&
-//         rect.bottom <= (window.innerHeight || covers.clientHeight) &&
-//         rect.right <= (window.innerWidth || covers.clientWidth)
-//     );
-// }
-
- // Виберіть елемент, який ви хочете відстежувати
-const element = document.querySelector('.section.covers');
-// Функція для перевірки, чи елемент видимий в вьюпорті
 function isElementInViewport(el) {
     const rect = el.getBoundingClientRect();
     return (
@@ -74,15 +50,14 @@ function isElementInViewport(el) {
         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
 }
-// Перевірка, чи елемент видимий при завантаженні сторінки
-// if (isElementInViewport(element)) {
-//     console.log('Елемент видимий в вьюпорті');
-// } else {
-//     console.log('Елемент не видимий в вьюпорті');
-// }
-// Слухач подій для перевірки, чи елемент видимий при прокрутці або зміні розміру вікна
-window.visualViewport.addEventListener('scroll', () => {
-    console.log("scroll!")
+
+if (isElementInViewport(element)) {
+    console.log('Елемент видимий в вьюпорті');
+} else {
+    console.log('Елемент не видимий в вьюпорті');
+}
+
+window.visualViewport.addEventListener('scroll', function () {
     if (isElementInViewport(element)) {
         console.log('Елемент видимий в вьюпорті');
     } else {
