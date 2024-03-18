7be8209792d3d79394d66f7f1e7b0c74971059ab
Viktoria <viki.boss10@gmail.com>
2024-03-18 15:54:07 +0200

    add projects section interactive slider
    this commit adds js and some styles for the slider in the projects section

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
-// import Swiper from 'swiper';
-// import { Navigation, Pagination } from 'swiper/modules';
+import Swiper from 'swiper';
+import { Navigation, Keyboard } from 'swiper/modules';
+import { projectsSwiper } from './js/projects'
 
+projectsSwiper.on('keyPress', (projectsSwiper, keyCode) => {
+  if (keyCode === 9) {
+    projectsSwiper.slideNext();
+  }
+});
