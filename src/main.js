b2f878c776ac5ca5d95b5b2b8d8d3649ac7c946a
MayaTaran <145223827+MayaTaran@users.noreply.github.com>
2024-03-18 19:45:45 +0200

    add export functions and some corrections in css

--- src/main.js
+++ src/main.js
@@ -15,3 +15,27 @@ axios.defaults.baseURL = 'https://portfolio-js.b.goit.study/api';
 
 renderSlide(); 
   
+//JS code for cection "Work together"
+import { formEmailInput } from './js/work-together';
+const formEmail = document.getElementById('mail');
+formEmail.addEventListener('input', formEmailInput);
+
+import { formCommentlInput } from './js/work-together';
+const formComment = document.getElementById('comment');
+formComment.addEventListener('input', formCommentlInput);
+
+import { closeModalWindow } from './js/work-together';
+const buttonClose = document.querySelector(".button-close");
+buttonClose.addEventListener('click', closeModalWindow);
+
+import { submitForm} from './js/work-together';
+const form = document.querySelector("form");
+form.addEventListener('submit',submitForm);
+
+import { backdropeClose} from './js/work-together';
+const backdrope = document.querySelector(".backdrope");
+backdrope.addEventListener('click', backdropeClose);
+
+import { escapeClose} from './js/work-together';
+document.addEventListener('keydown', escapeClose);
+// End JS code for cection "Work together"
